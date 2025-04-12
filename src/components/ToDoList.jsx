cd47b5b430bed59374af7f844a3b769915c34806
Samuele Deriu <deriusamuele@gmail.com>
2025-04-12 13:45:13 +0200

    aggiunta logica per ordinare le attivita(tutte, attive, completate)

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import ToDoItem from "./ToDoItem";
 
 export default function ToDoList({ listItems, setDoneItem, deleteItem }) {
+  const [filterList, setFilterList] = useState("");
+
+  const handleFilterList = (e) => {
+    setFilterList(e.target.value);
+  };
+
+  const filteredList = () => {
+    switch (filterList) {
+      case "attive":
+        return listItems.filter((item) => !item.done);
+      case "completate":
+        return listItems.filter((item) => item.done);
+      default:
+        return listItems;
+    }
+  };
   return (
     <>
       {listItems.length === 0 ? (
@@ -10,12 +27,35 @@ export default function ToDoList({ listItems, setDoneItem, deleteItem }) {
         </p>
       ) : (
         <div>
-          <h2 className="text-4xl mb-10 text-indigo-500 text-left font-bold">
-            📑 Elenco attività
-          </h2>
+          <div className="flex flex-row mb-10 p-3  justify-between items-end">
+            <h2 className="text-4xl  text-indigo-500 text-left font-bold">
+              📑 Elenco attività
+            </h2>
+            <div className="mr-3">
+              <select
+                className="p-2 font-bold rounded-md"
+                value={filterList}
+                onChange={handleFilterList}
+              >
+                <option className="bg-zinc-900" value="">
+                  Ordina per
+                </option>
+                <option className="bg-zinc-900" value="all" autoFocus>
+                  Tutte{" "}
+                </option>
+                <option className="bg-zinc-900" value="attive">
+                  Attive
+                </option>
+                <option className="bg-zinc-900" value="completate">
+                  Completate
+                </option>
+              </select>
+            </div>
+          </div>
+
           <div className=" mb-10 ">
             <ul className="grid grid-cols-3 gap-5">
-              {listItems.map((item) => (
+              {filteredList().map((item) => (
                 <li key={item.id}>
                   <ToDoItem
                     item={item}
